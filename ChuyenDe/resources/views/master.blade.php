6b463a521fdae60d39a126e3151205a1a49b0f2d
thich02 <52402192+thich02@users.noreply.github.com>
2019-08-06 18:53:06 +0700

    sua

--- ChuyenDe/resources/views/master.blade.php
+++ ChuyenDe/resources/views/master.blade.php
@@ -158,7 +158,6 @@
 
 
 
-
         
 
     @yield('conten')
--- ChuyenDe/routes/web.php
+++ ChuyenDe/routes/web.php
@@ -19,3 +19,7 @@ Route::get('Index',[
 	'as'=>'trang-chu',
 	'uses'=>'pageController@getIndex']);
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
